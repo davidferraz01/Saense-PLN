d32aa5a03243596c56a4c57864d1d283177ce8d3
David Ferraz <64922774+0xdferraz@users.noreply.github.com>
2022-02-18 11:53:13 -0300

    Add files via upload

--- handle_page.py
+++ handle_page.py
@@ -24,24 +24,37 @@ class HandlePage:
         soup = BeautifulSoup(response.content, 'html.parser')
         self.soup = soup
 
+    def cleanText(self, text):
+        text = text.replace('\n', ' ')
+        cleanText = []
+        # Parametro criado para auxiliar a remover referencias do  texto #
+        ref = False
+        for ch in text:
+            if ref:
+                pass
+            elif ch == "[":
+                ref = True
+            elif ch == "]":
+                ref = False
+            else:
+                nfkd = unicodedata.normalize('NFKD', ch)
+                filter = re.sub('[^A-z 1-9 , . ( ) / : - -\\\]', '', nfkd)
+                char = u"".join([c for c in filter if not unicodedata.combining(c)])
+                cleanText.append(char)
+        return ''.join(cleanText)
+
     def getTitulo(self):
         titulo = self.soup.title.string.split("–")[0].rstrip().lower()
-        cleanText = []
-        for ch in titulo:
-            nfkd = unicodedata.normalize('NFKD', ch)
-            filter = re.sub('[^a-z \\\]', '', nfkd)
-            char = u"".join([c for c in filter if not unicodedata.combining(c)])
-            (cleanText).append(char)
-        self.titulo =  ''.join(cleanText)        
+        self.titulo = self.cleanText(titulo)       
 
     def getTexto(self):
         bodydiv = self.soup.body.find("div",{"itemprop":"articleBody"})
         bodycontent = bodydiv.find_all(lambda tag: tag.name == 'p' and not tag.attrs)
-        bodycontentfinal = self.titulo + "\n"
-        for p in bodycontent:
-            bodycontentfinal += p.getText() + " "
-        self.conteudo = bodycontentfinal
-
+        bodycontentfinal = ""
+        for p in bodycontent[1:]:
+            bodycontentfinal += p.get_text() + " "
+        self.conteudo = self.cleanText(bodycontentfinal)
+        
     def getAutor(self):
         try:
             bodydiv = self.soup.body.find("div",{"itemprop":"articleBody"})
